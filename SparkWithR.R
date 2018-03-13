a7c1fe9f366b44d2f3ecb4de87aec173a8a22e2a
Renzo Elvis Cerron Tome <geckolml@users.noreply.github.com>
2018-03-12 23:11:22 -0500

    Spark Code with R

---
+++ SparkWithR.R
@@ -0,0 +1,80 @@
+# SparkWithR.R
+# from blog: http://blog.godatadriven.com/sparkr-just-got-better.html
+.libPaths(c(.libPaths(), '/root/spark/R/lib'))
+Sys.setenv(SPARK_HOME = '/root/spark')
+Sys.setenv(PATH = paste(Sys.getenv(c('PATH')), '/root/spark/bin', sep=':'))
+
+library(SparkR)
+library(magrittr) # Pipes
+# initialize the Spark Context
+sc <- sparkR.init()
+sqlContext <- sparkRSQL.init(sc)
+
+bank <- read.csv("/home/renzo/bank.csv",sep = ";") %>% 
+  data.frame()
+
+# create a data frame using the createDataFrame object
+df <- createDataFrame(sqlContext, bank) 
+head(df)
+
+# bancos <- df %>% 
+# collect()
+
+model <- glm(age ~ balance + education, data = df,
+             family = "gaussian")
+summary(model)
+
+predictions <- predict(model, newData = df)
+
+head(predictions)
+
+predictions %>% 
+  SparkR::select("age","prediction") %>% 
+  head
+
+df %>% head()
+
+df %>% 
+  SparkR::filter("balance>7") %>% 
+  SparkR::head()
+
+df %>% 
+  SparkR::groupBy("education") %>% 
+  SparkR::summarize(MinAge=min(df$age)) %>% 
+  head
+
+df %>% 
+  SparkR::groupBy("education") %>% 
+  SparkR::summarize(Count=n()) %>% 
+  head
+
+head(SparkR::arrange(SparkR::agg(SparkR::groupBy(df,df$age),
+                                 total = sum(df$balance)),
+                     df$age))
+
+df %>% 
+  SparkR::groupBy(df$age) %>% 
+  SparkR::agg(total=sum(df$balance)) %>% 
+  SparkR::arrange(df$age) %>% 
+  head()
+
+
+
+# SparkSQL ----------------------------------------------------------------
+
+registerTempTable(df,"dfBank")
+
+jobs <- SparkR::sql(sqlContext,"SELECT job from dfBank")
+
+jobs %>% 
+  head
+
+result2 <- SparkR::sql(sqlContext,
+                       "SELECT age,job,balance,loan from dfBank WHERE balance>=1000")
+
+sqlContext %>% 
+  SparkR::sql("SELECT age,job,balance,loan from dfBank WHERE balance>=1000") %>% 
+  SparkR::collect() %>% 
+  count()
+
+
